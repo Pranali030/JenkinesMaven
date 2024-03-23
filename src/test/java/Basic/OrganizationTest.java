709b7ec6d8b3e99ea9e403abca4722e36b2fa9ac
pranali dhongade <pranalidhongade126@gmail.com>
2024-03-23 15:00:48 +0530

    Click commit push

---
+++ src/test/java/Basic/OrganizationTest.java
@@ -0,0 +1,24 @@
+package Basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	
+	@Test
+	public void CreateOrganizationTest() {
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("http://localhost:8888/");
+		
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("root");
+		driver.findElement(By.id("submitButton")).click();
+				
+				
+				
+	}
+
+}
